bcde7dc7b51d1789498cb6435b21e01253abd197
karthik-ziffity <karthikeyan.indran@ziffity.com>
2025-07-14 15:35:21 +0530

    Initial commit 1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1d1fb7020d9248f5"; }
+/******/ 		__webpack_require__.h = function() { return "c0f921d63a45f187"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.36cc243b5a1400ef.hot-update.js
+++ .next/static/webpack/webpack.36cc243b5a1400ef.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6a360973367e67b6"; }
+/******/ 	__webpack_require__.h = function() { return "2f473f62faf66091"; }
 /******/ }();
 /******/ 
 /******/ }
